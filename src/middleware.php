2de40b3ee5dbcce4792e3afacf022e878c174a0a
Ramon Santos <ramonsantos@bemol.com.br>
2019-02-26 17:38:27 -0400

    middleware ok

--- src/middleware.php
+++ src/middleware.php
@@ -4,13 +4,12 @@
 // e.g: $app->add(new \Slim\Csrf\Guard);
 use Slim\Http\Request;
 use Slim\Http\Response;
-//use Callable;
 
 class Middleware{
     public function __invoke(Request $request, Response $response, $next){
-        $request->getBody()->write('BEFORE');
+        $response->getBody()->write('BEFORE');
         $response = $next($request, $response);
-        $request->getBody()->write('AFTER');
+        $response->getBody()->write('AFTER');
 
         return $response;
     }
